d95c1afb67251552e292e614bafb76f6dfa0d23e
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-08-14 16:30:49 +0500

    4.30

--- final/f1.c
+++ final/f1.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 typedef struct products
 {
     int prodNum;
@@ -7,7 +7,7 @@ typedef struct products
     float retail;
     int quant;
     float amount;
-}Products;
+} Products;
 void display(Products p[])
 {
     printf("\nPrepared by: Aneeq Khurram");
@@ -15,59 +15,70 @@ void display(Products p[])
 
     printf("\nProduct Number Product Name\tRetail Price in Rs.\tQuantity");
 
-
-
-
     for (int i = 0; i < 5; i++)
     {
-        if (i==1||i==2)
+        if (i == 1 || i == 2)
         {
             printf("\n%5d\t\t%s\t%.2f\t\t%d", p[i].prodNum, p[i].prodName, p[i].retail, p[i].quant);
         }
-        else if (i==0)
+        else if (i == 0)
         {
-            printf("\n%5d\t\t%s\t\t\t%.2f\t\t%d", p[i].prodNum, p[i].prodName, p[i].retail, p[i].quant);
+            printf("\n%5d\t\t%s\t\t\t %.2f\t\t%d", p[i].prodNum, p[i].prodName, p[i].retail, p[i].quant);
         }
         else
         {
             printf("\n%5d\t\t%s\t\t%.2f\t\t%d", p[i].prodNum, p[i].prodName, p[i].retail, p[i].quant);
-
         }
-
-
     }
-
 }
 int main()
 {
-    int numOfProd, prodnum[5], quantity[5];
-    Products prod[5]={ { 101, "Soap", 60.00, 10 }, { 102, "Liquid Hand Wash", 200.00, 20 },
-        { 103, "Liquid Dish Wash", 150.00, 30 }, { 104, "Table Cleaner", 350.00, 40 },
-        { 105, "Hand Sanitizer", 100.00, 50 } };
+    int numOfProd, prodnum[5], quantity[5], temp[5];
+
+    Products prod[5] = {{101, "Soap", 60.00, 10}, {102, "Liquid Hand Wash", 200.00, 20}, {103, "Liquid Dish Wash", 150.00, 30}, {104, "Table Cleaner", 350.00, 40}, {105, "Hand Sanitizer", 100.00, 50}};
+Restart:
+
     display(prod);
     printf("\nEnter the number of products you want to purchase:");
     printf("\nNOTE: You can only purchase at most 5 products: ");
-
-    do {
+    float totalBill = 0;
+    int flag = 0;
+    do
+    {
         fflush(stdin);
         scanf("%d", &numOfProd);
-        if (numOfProd>5)
+        if (numOfProd > 5)
         {
             printf("\nTry Again.");
             printf("\nNOTE: You can only purchase at most 5 products: ");
-
         }
 
-    } while (numOfProd>5);
+    } while (numOfProd > 5);
     for (int i = 0; i < numOfProd; i++)
     {
-        printf("\nEnter product number : ", i+1);
+        printf("\nEnter product number : ", i + 1);
         fflush(stdin);
         scanf("%d", &prodnum[i]);
-        printf("\nEnter quantity : ");
-        fflush(stdin);
-        scanf("%d", &quantity[i]);
-
+        for (int j = 0; j < 5; j++)
+        {
+            if (prodnum[i] == prod[j].prodNum)
+            {
+                printf("\nEnter quantity : ");
+                fflush(stdin);
+                scanf("%d", &quantity[i]);
+                flag = 0;
+                break;
+            }
+            else
+            {
+                flag = 1;
+            }
+        }
+        if (flag != 0)
+        {
+            printf("\nYou entered wrong product number.\nTry Again.");
+            i--;
+        }
     }
     for (int i = 0; i < 5; i++)
     {
@@ -75,23 +86,63 @@ int main()
         for (int j = 0; j < 5; j++)
         {
 
-
-
-            if (prodnum[i]==prod[j].prodNum)
+            if (prodnum[i] == prod[j].prodNum)
             {
-                if (quantity[i]>prod[j].quant)
+                if (quantity[i] > prod[j].quant)
                 {
                     printf("\nStock was isnufficient to complete order %d.", prodnum[i]);
-
+                    temp[j] = prod[j].quant;
+                    quantity[i] = prod[j].quant;
+                    prod[j].amount = quantity[i] * prod[j].retail;
+                    prod[j].quant = 0;
+                    totalBill = totalBill + prod[j].amount;
                 }
-
+                else
+                {
+                    temp[j] = quantity[i];
+                    prod[j].quant = prod[j].quant - quantity[i];
+                    prod[j].amount = quantity[i] * prod[j].retail;
+                    totalBill = totalBill + prod[j].amount;
+                }
             }
+        }
+    }
+    printf("\nProduct Number Product Name\tRetail Price in Rs.\tQuantity\tAmount");
+    for (int i = 0; i < numOfProd; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
 
+            if (prodnum[i] == prod[j].prodNum && temp[j] != 0)
+            {
+
+                if (j == 1 || j == 2)
+                {
+                    printf("\n%5d\t\t%s\t%.2f\t\t%d\t\t%.2f", prod[j].prodNum, prod[j].prodName, prod[j].retail, temp[j], prod[j].amount);
+                }
+                else if (j == 0)
+                {
+                    printf("\n%5d\t\t%s\t\t\t %.2f\t\t%d\t\t%.2f", prod[j].prodNum, prod[j].prodName, prod[j].retail, temp[j], prod[j].amount);
+                }
+                else
+                {
+                    printf("\n%5d\t\t%s\t\t%.2f\t\t%d\t\t%.2f", prod[j].prodNum, prod[j].prodName, prod[j].retail, temp[j], prod[j].amount);
+                }
+            }
         }
     }
 
+    printf("\nTotal Bill = %.2f", totalBill);
 
-
-
-
+    char choice;
+    printf("\nPress 'y' to continue shopping: ");
+    scanf("\n%c", &choice);
+    if (choice == 'y')
+    {
+        goto Restart;
+    }
+    else
+    {
+        printf("\nHave a nice day :)");
+    }
 }
