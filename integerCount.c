1efb1a971918439345c0b5f206d68f07f179b153
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-08-26 21:21:45 +0500

    9.21

--- integerCount.c
+++ integerCount.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    int num, count = 0, temp;
+
+    int num, count, temp;
+Restart:
+    count = 0;
     printf("\nEnter an integer to count digits: ");
     scanf("%d", &num);
+    if (num < 0)
+    {
+        num = -num;
+    }
+
+    if (num == 0)
+    {
+        count++;
+    }
     while (num != 0)
     {
         temp = num % 10;
@@ -14,5 +27,14 @@ int main()
         }
         num = num / 10;
     }
+
     printf("\nTotal digits are : %d", count);
+    char choice;
+    printf("\nDo you want to continue (y/n): ");
+    fflush(stdin);
+    scanf("%c", &choice);
+    if (choice == 'y')
+    {
+        goto Restart;
+    }
 }
