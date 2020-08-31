dcbb04622534547af14b33ebb69fb572ab12d955
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-08-31 21:45:56 +0500

    9.45

--- power.c
+++ power.c
@@ -2,28 +2,17 @@
 int main()
 {
     int num, power;
-    long long int sum;
+    long long int sum = 1;
     printf("\nEnter a number: ");
     scanf("%d", &num);
     printf("\nEnter a power: ");
     scanf("%d", &power);
-    if (power == 0)
+
+    for (int i = 1; i <= power; i++)
     {
-        printf("\nThe answer is 1");
+
+        sum = sum * num;
     }
-    else
-    {
-        for (int i = 1; i <= power; i++)
-        {
-            if (i == 1)
-            {
-                sum = i * num;
-            }
-            else
-            {
-                sum = sum * num;
-            }
-        }
-    }
+
     printf("\nThe answer is %lld", sum);
 }
