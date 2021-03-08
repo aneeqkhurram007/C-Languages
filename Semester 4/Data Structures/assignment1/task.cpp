3d76d443cc66697d1041d96021423c5b340fb86a
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-08 18:35:16 +0500

    brackets improved

--- Semester 4/Data Structures/assignment1/task.cpp
+++ Semester 4/Data Structures/assignment1/task.cpp
@@ -4,29 +4,36 @@ using namespace std;
 int SIZE = 50;
 int main()
 {
-    cout << "\tInfix To PostFix" << endl;
+    cout << "\t\tInfix To PostFix" << endl;
     cout << "Prepared By: Aneeq Khurram\t RegId: FA19-BCS-145" << endl;
+
     int top = -1;
+    float temp = 0;
+    int LB = 0; //Left Bracket;
+    int RB = 0; //Right Bracket;
+
     char *infix = new char[SIZE]{0};
     char *postfix = new char[SIZE]{0};
     char *stack = new char[SIZE]{0};
+
     float result[SIZE] = {0};
-    cout << "Note: Must enclose your expression within brackets." << endl;
+
+    cout << "\nNote: Must enclose your expression within brackets." << endl;
+
     cout << "Enter an expression: ";
     cin.getline(infix, SIZE);
-    float temp = 0;
 
     for (int i = 0, p = 0; infix[i] != '\0'; i++)
     {
         if (infix[i] >= 40 && infix[i] <= 47)
         {
             top++;
-
             stack[top] = infix[i];
 
             if (stack[top] == 41)
             {
 
+                RB++;
                 // switch (stack[top - 1])
                 // {
                 // case 42:
@@ -53,13 +60,17 @@ int main()
                 //     break;
                 // }
                 postfix[p] = stack[top - 1];
-
                 p++;
 
                 stack[top] = '\0';
                 stack[top - 1] = '\0';
                 stack[top - 2] = '\0';
+
                 top -= 3;
+            }
+            else if (stack[top] == 40)
+            {
+                LB++;
             }
         }
         else
@@ -68,6 +79,13 @@ int main()
             p++;
         }
     }
-    cout << "Stack: " << stack << endl;
-    cout << "PostFix Expression: " << postfix << endl;
+    if (RB == LB)
+    {
+        cout << "Stack: " << stack << endl;
+        cout << "PostFix Expression: " << postfix << endl;
+    }
+    else
+    {
+        cout << "Invalid set of brackets." << endl;
+    }
 }
