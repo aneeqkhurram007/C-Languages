c418f27f3716da9d7158e3cf2e6a6c3751f29c62
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-08 18:23:32 +0500

    6.23

--- Semester 4/Data Structures/assignment1/task.cpp
+++ Semester 4/Data Structures/assignment1/task.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int SIZE = 50;
 int main()
 {
-
+    cout << "\tInfix To PostFix" << endl;
+    cout << "Prepared By: Aneeq Khurram\t RegId: FA19-BCS-145" << endl;
     int top = -1;
     char *infix = new char[SIZE]{0};
     char *postfix = new char[SIZE]{0};
     char *stack = new char[SIZE]{0};
     float result[SIZE] = {0};
+    cout << "Note: Must enclose your expression within brackets." << endl;
     cout << "Enter an expression: ";
     cin.getline(infix, SIZE);
     float temp = 0;
+
     for (int i = 0, p = 0; infix[i] != '\0'; i++)
     {
         if (infix[i] >= 40 && infix[i] <= 47)
@@ -20,7 +23,6 @@ int main()
             top++;
 
             stack[top] = infix[i];
-            cout << stack[top] << top << endl;
 
             if (stack[top] == 41)
             {
@@ -53,6 +55,7 @@ int main()
                 postfix[p] = stack[top - 1];
 
                 p++;
+
                 stack[top] = '\0';
                 stack[top - 1] = '\0';
                 stack[top - 2] = '\0';
@@ -65,6 +68,6 @@ int main()
             p++;
         }
     }
-    cout << stack << endl;
-    cout << postfix;
+    cout << "Stack: " << stack << endl;
+    cout << "PostFix Expression: " << postfix << endl;
 }
