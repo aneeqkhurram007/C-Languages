c34adcf53b991d78a2702824aa5dfdd264f4445b
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-09 17:52:59 +0500

    Modular Code

--- Semester 4/Data Structures/assignment1/task.cpp
+++ Semester 4/Data Structures/assignment1/task.cpp
@@ -1,7 +1,158 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int SIZE = 50;
+int top = -1;
+
+char *pop(char *stack, char element)
+{
+    top--;
+    char *temp = new char[SIZE];
+    for (int i = 0, j = 0; i < top; i++)
+    {
+        if (stack[i] != element)
+        {
+            temp[j] = stack[i];
+            j++;
+        }
+    }
+    delete[] stack;
+    return temp;
+}
+char *push(char *stack, char element)
+{
+    top++;
+    int i;
+    char *temp = new char[SIZE];
+    for (i = 0; i < top - 1; i++)
+    {
+
+        temp[i] = stack[i];
+    }
+    temp[top - 1] = element;
+    delete[] stack;
+    return temp;
+}
+bool isEmpty()
+{
+    return top == -1;
+}
+bool isFull()
+{
+    return top == SIZE;
+}
+bool isOperator(char Operator)
+{
+    return Operator >= 40 && Operator <= 47;
+}
+void stringInitializer(char *infix)
+{
+    cout << "\nNote: Must enclose your expression within brackets (...expression)." << endl;
+    cout << "Enter an expression: ";
+    fflush(stdin);
+    cin.getline(infix, SIZE);
+}
+int stringSize(char *infix)
+{
+    int tempSize = 0;
+    for (int i = 0; infix[i] != 0; i++)
+    {
+        ++tempSize;
+    }
+    return tempSize;
+}
+void processing(char *infix, char *postfix, char *stack, int *LB, int *RB)
+{
+    for (int i = 0, p = 0; infix[i] != '\0'; i++)
+    {
+        if (isOperator(infix[i]))
+        {
+            top++;
+            stack[top] = infix[i];
+
+            if (stack[top] == 41)
+            {
+
+                RB++;
+                postfix[p] = stack[top - 1];
+                p++;
+
+                stack[top] = '\0';
+                stack[top - 1] = '\0';
+                stack[top - 2] = '\0';
+
+                top -= 3;
+            }
+            else if (stack[top] == 40)
+            {
+                LB++;
+            }
+        }
+        else
+        {
+            postfix[p] = infix[i];
+            p++;
+        }
+    }
+}
+void resultsProcessing(char *postfix)
+{
+    int *temp = new int[SIZE];
+    bool flag = false;
+    for (int i = 0; postfix[i] != '\0'; i++)
+    {
+        if (!(postfix[i] >= 40 && postfix[i] <= 57))
+        {
+
+            flag = true;
+            break;
+        }
+    }
+    if (!flag)
+    {
+        for (int i = 0, j = 0; postfix[i] != '\0'; i++)
+        {
+            if (isOperator(postfix[i]))
+            {
+                switch (postfix[i])
+                {
+                case 42:
+
+                    temp[j - 2] = temp[j - 2] * temp[j - 1];
+                    j -= 1;
+
+                    break;
+                case 43:
+                    temp[j - 2] = temp[j - 2] + temp[j - 1];
+                    j -= 1;
+                    break;
+                case 45:
+                    temp[j - 2] = temp[j - 2] - temp[j - 1];
+                    j -= 1;
+                    break;
+                case 47:
+                    temp[j - 2] = temp[j - 2] / temp[j - 1];
+                    j -= 1;
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            else
+            {
+                temp[j] = postfix[i] - 48;
+                j++;
+            }
+        }
+        cout << "Result: " << temp[0] << endl;
+    }
+    else
+    {
+        cout << "No Result for Undefined variables." << endl;
+    }
+}
 int main()
 {
     cout << "\t\tInfix To PostFix" << endl;
@@ -11,7 +162,6 @@ int main()
     do
     {
 
-        int top = -1;
         int LB = 0;
         int RB = 0;
 
@@ -21,107 +171,16 @@ int main()
 
         float result[SIZE] = {0};
 
-        cout << "\nNote: Must enclose your expression within brackets (...expression)." << endl;
+        stringInitializer(infix);
 
-        cout << "Enter an expression: ";
-        fflush(stdin);
-        cin.getline(infix, SIZE);
+        int tempSize = stringSize(infix);
 
-        int tempSize = 0;
-        for (int i = 0; infix[i] != 0; i++)
-        {
-            ++tempSize;
-        }
+        processing(infix, postfix, stack, &LB, &RB);
 
-        for (int i = 0, p = 0; infix[i] != '\0'; i++)
-        {
-            if (infix[i] >= 40 && infix[i] <= 47)
-            {
-                top++;
-                stack[top] = infix[i];
-
-                if (stack[top] == 41)
-                {
-
-                    RB++;
-                    postfix[p] = stack[top - 1];
-                    p++;
-
-                    stack[top] = '\0';
-                    stack[top - 1] = '\0';
-                    stack[top - 2] = '\0';
-
-                    top -= 3;
-                }
-                else if (stack[top] == 40)
-                {
-                    LB++;
-                }
-            }
-            else
-            {
-                postfix[p] = infix[i];
-                p++;
-            }
-        }
         if (RB == LB && infix[tempSize - 1] == 41 && infix[0] == 40)
         {
-            cout << "Stack: " << endl;
             cout << "PostFix Expression: " << postfix << endl;
-            int *temp = new int[SIZE];
-            int flag = 0;
-            for (int i = 0; postfix[i] != '\0'; i++)
-            {
-                if (!(postfix[i] >= 48 && postfix[i] <= 57))
-                {
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0)
-            {
-                for (int i = 0, j = 0; postfix[i] != '\0'; i++)
-                {
-                    if (postfix[i] == '+' || postfix[i] == '*' || postfix[i] == '-' || postfix[i] == '/')
-                    {
-                        switch (postfix[i])
-                        {
-                        case 42:
-
-                            temp[j - 2] = temp[j - 2] * temp[j - 1];
-                            j -= 1;
-
-                            break;
-                        case 43:
-                            temp[j - 2] = temp[j - 2] + temp[j - 1];
-                            j -= 1;
-                            break;
-                        case 45:
-                            temp[j - 2] = temp[j - 2] - temp[j - 1];
-                            j -= 1;
-                            break;
-                        case 47:
-                            temp[j - 2] = temp[j - 2] / temp[j - 1];
-                            j -= 1;
-                            break;
-                            break;
-
-                        default:
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        temp[j] = postfix[i] - 48;
-                        j++;
-                    }
-                }
-                cout << "Result: " << temp[0] << endl;
-            }
-            else
-            {
-                cout << "No Result for Undefined variables." << endl;
-            }
+            resultsProcessing(postfix);
         }
         else
         {
