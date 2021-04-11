42e006ac3d5778840d1e07c8b0eaac3914868259
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-04-11 19:10:15 +0500

    fixed

--- Semester 4/Data Structures/S1/stack.cpp
+++ Semester 4/Data Structures/S1/stack.cpp
@@ -9,8 +9,6 @@ public:
     stack *next;
 };
 
-stack *top = NULL;
-
 bool isFull(stack *top)
 {
     stack *p = new stack();
@@ -26,24 +24,26 @@ stack *push(stack *top, int x)
     {
         cout << "Stack Overflow" << endl;
     }
+
     else
     {
         stack *n = new stack();
         n->data = x;
         n->next = top;
         top = n;
-        return top;
     }
+    return top;
 }
 
-int pop(stack *tp)
+int pop(stack *top)
 {
-    if (isEmpty(tp))
+    if (isEmpty(top))
     {
         cout << "Stack Underflow" << endl;
     }
     else
     {
+        stack *tp = top;
         stack *n = tp;
         top = tp->next;
         int x = n->data;
@@ -51,64 +51,75 @@ int pop(stack *tp)
         return x;
     }
 }
-void traversal(stack *ptr)
+void traversal(stack *ptr, int TOP)
 {
     int count = 0;
-    while (ptr != NULL)
+    int flag = 0;
+    stack *p = ptr;
+
+    while (p != NULL)
     {
-        cout << "Element: " << ptr->data << endl;
-        ptr = ptr->next;
+        stack *temp = ptr;
+        for (int i = 0; i < TOP - count; i++)
+        {
+            temp = temp->next;
+        }
+        if (p->data != temp->data)
+        {
+            flag = 1;
+            break;
+        }
+
+        p = p->next;
+        count++;
+    }
+    if (flag == 0)
+    {
+        cout << "\nThe expression you entered is a palindrome" << endl;
+    }
+    else
+    {
+        cout << "\nThe expression you entered is not a palindrome" << endl;
     }
 }
 int main()
 {
     cout << "Prepared By: Aneeq Khurram \tRegId: FA19-BCS-145" << endl;
-    char *s = new char(50);
+    char *s;
     char choice;
     do
     {
+        stack *top = NULL;
+        s = new char(50);
+        int TOP = -1;
         cout << "\nPlease enter the input expression: ";
         fflush(stdin);
         cin.getline(s, 50);
-        int count = 0;
+
         for (int i = 0; s[i] != '\0'; i++)
         {
             if (s[i] >= 65 && s[i] <= 90)
             {
                 s[i] += 32;
+                top = push(top, s[i]);
+                TOP++;
             }
-            if (s[i] >= 97 && s[i] <= 122)
+            else if (s[i] >= 97 && s[i] <= 122)
             {
                 top = push(top, s[i]);
+                TOP++;
             }
-
-            count++;
-        }
-
-        int flag = 0;
-        for (int i = 0; s[i] != '\0'; i++)
-        {
-            if (s[i] == s[count - i - 1])
+            else if (s[i] >= 48 && s[i] <= 57)
             {
-                flag = 1;
-            }
-            else
-            {
-                flag = 0;
-                break;
+                top = push(top, s[i]);
+                TOP++;
             }
         }
-        if (flag == 1)
-        {
-            cout << "\nThe expression you entered is a palindrome" << endl;
-        }
-        else
-        {
-            cout << "\nThe expression you entered is not a palindrome" << endl;
-        }
+        traversal(top, TOP);
 
         cout << "\nDo you want to continue: ";
         cin >> choice;
+        delete top;
 
     } while (choice == 'y' || choice == 'Y');
     cout << "\nThank you for using this palindrome evaluator" << endl;
