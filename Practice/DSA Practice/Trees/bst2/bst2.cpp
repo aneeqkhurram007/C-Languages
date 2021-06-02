e786099b9bd3422c95535a3cb9156d8e22c91dcf
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-06-02 23:48:24 +0500

    11.48

---
+++ Practice/DSA Practice/Trees/bst2/bst2.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class bst2
+{
+
+public:
+    int data;
+    bst2 *left = NULL;
+    bst2 *right = NULL;
+};
+bst2 *insert(bst2 *root, int data)
+{
+    if (root == NULL)
+    {
+        root = new bst2;
+        root->data = data;
+        root->left = root->right = NULL;
+    }
+    else if (data > root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+    else
+    {
+        root->left = insert(root->left, data);
+    }
+    return root;
+}
+void inOrder(bst2 *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+void preOrder(bst2 *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void postOrder(bst2 *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+int height(bst2 *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lheight = height(root->left);
+    int rheight = height(root->right);
+    if (lheight > rheight)
+    {
+        return lheight + 1;
+    }
+    else
+    {
+        return rheight + 1;
+    }
+}
+void printCurrentLevel(bst2 *root, int height)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (height == 1)
+    {
+        cout << root->data << " ";
+    }
+    else if (height > 1)
+    {
+        printCurrentLevel(root->left, height - 1);
+        printCurrentLevel(root->right, height - 1);
+    }
+}
+void printLevelOrder(bst2 *root)
+{
+    int h = height(root);
+    for (int i = 1; i <= h; i++)
+    {
+        printCurrentLevel(root, i);
+    }
+}
+
+void printLevelQueues(bst2 *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<bst2 *> q;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        bst2 *node = q.front();
+        cout << node->data << " ";
+        q.pop();
+        if (node->left != NULL)
+        {
+            q.push(node->left);
+        }
+        if (node->right != NULL)
+        {
+            q.push(node->right);
+        }
+    }
+}
+void parentNode(bst2 *root, int data, int parent)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (root->data == data)
+    {
+        cout << parent;
+    }
+    else
+    {
+        parentNode(root->left, data, root->data);
+        parentNode(root->right, data, root->data);
+    }
+}
+int levelOfNode(bst2 *root, int node)
+{
+    int level = 0;
+    do
+    {
+        level++;
+        if (root->data == node)
+        {
+            cout << "Level of Node: " << level;
+            break;
+        }
+        else if (node > root->data)
+        {
+            root = root->right;
+        }
+        else if (node < root->data)
+        {
+            root = root->left;
+        }
+
+    } while (root != NULL);
+    return level;
+}
+int main()
+{
+    bst2 *root = NULL;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+    cout << "InOrder: ";
+    inOrder(root);
+    cout << "\nPreOrder: ";
+    preOrder(root);
+    cout << "\nPostOrder: ";
+    postOrder(root);
+    cout << "\nLevel Order: ";
+    printLevelOrder(root);
+    cout << "\n";
+    printLevelQueues(root);
+    cout << "\nParent: ";
+    parentNode(root, 80, -1);
+    // levelOfNode(root, 80);
+
+    return 0;
+}
